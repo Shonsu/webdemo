c4b4b3eab6291fdcf5af233fb45efd96dcb3dde7
Shonsu82 <shonsu82@gmail.com>
2022-05-24 18:37:34 +0200

    delete unnesesery files

--- src/main/java/pl/shonsu/DateServlet.java
+++ src/main/java/pl/shonsu/DateServlet.java
@@ -2,20 +2,24 @@ package pl.shonsu;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/DateServlet")
+@WebServlet(name = "Date", urlPatterns = {"/date"})
 public class DateServlet extends HttpServlet{
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        //DateTime now = DateTime.now();
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
         PrintWriter responseOutput = response.getWriter();
-        responseOutput.append("<html><body>" + "TEST" + "</body></html>");
+        responseOutput.append("<html><body>" + dtf.format(now).toString() + "</body></html>");
     }
     
 }
