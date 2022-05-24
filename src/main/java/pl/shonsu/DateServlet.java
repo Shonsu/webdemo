186ef7fc0215e510a0d992bb55053aea4a0552cd
Shonsu82 <shonsu82@gmail.com>
2022-05-24 20:06:19 +0200

    changing jawax to jakarta for tomcat10

--- src/main/java/pl/shonsu/DateServlet.java
+++ src/main/java/pl/shonsu/DateServlet.java
@@ -5,11 +5,13 @@ import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+
 
 @WebServlet(name = "Date", urlPatterns = {"/date"})
 public class DateServlet extends HttpServlet{
